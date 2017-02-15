b2d022b1cc9b7c014972341070788bf5f9c7bc75
Jackson Pires <jackson.pires@gmail.com>
2017-02-15 15:43:46 +0000

    Render JSON com associações

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,7 +5,15 @@ class Contact < ApplicationRecord
     "Jackson Pires"
   end
 
+  def kind_desription
+    self.kind.description
+  end
+
   def as_json(options={})
-    super(methods: :author, root: true)
+    super(
+      root: true,
+      methods: [:kind_desription, :author],
+      include: { kind: { only: :description }}
+    )
   end
 end
