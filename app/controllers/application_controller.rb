1c030ea9890cdb96da793b379869b0fd8f907000
Jackson Pires <jackson.pires@gmail.com>
2017-03-29 23:34:48 +0000

    Media Types

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+  before_filter :ensure_json_request
+
+  def ensure_json_request
+    return if request.headers["Accept"] =~ /vnd\.api\+json/
+    render :nothing => true, :status => 406
+  end
+
 end
