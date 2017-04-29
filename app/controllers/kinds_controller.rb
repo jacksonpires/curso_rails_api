6b08ea6329e61d5fe57b4e6b38e621d376fa4319
Jackson Pires <jackson.pires@gmail.com>
2017-04-29 02:27:42 +0000

    Autenticação HTTP no Rails

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -1,8 +1,12 @@
 class KindsController < ApplicationController
 
-  include ActionController::HttpAuthentication::Basic::ControllerMethods
-  http_basic_authenticate_with name: "jack", password: "secret"
+  # include ActionController::HttpAuthentication::Basic::ControllerMethods
+  # http_basic_authenticate_with name: "jack", password: "secret"
 
+  include ActionController::HttpAuthentication::Digest::ControllerMethods
+  USERS = { "jack" => Digest::MD5.hexdigest(["jack","Application","secret"].join(":"))}
+
+  before_action :authenticate
   before_action :set_kind, only: [:show, :update, :destroy]
 
   # GET /kinds
@@ -55,5 +59,11 @@ class KindsController < ApplicationController
     # Only allow a trusted parameter "white list" through.
     def kind_params
       params.require(:kind).permit(:description)
+    end
+
+    def authenticate
+      authenticate_or_request_with_http_digest("Application") do |username|
+        USERS[username]
+      end
     end
 end
