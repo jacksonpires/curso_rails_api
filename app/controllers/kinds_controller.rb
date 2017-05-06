8cdcb3abe2e6d4c57f5ef8548df495f238f637a6
Jackson Pires <jackson.pires@gmail.com>
2017-05-06 20:54:12 +0000

    Autenticação baseada em Token (Rails)

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -1,10 +1,14 @@
 class KindsController < ApplicationController
 
+  TOKEN = "secret123"
+
   # include ActionController::HttpAuthentication::Basic::ControllerMethods
   # http_basic_authenticate_with name: "jack", password: "secret"
 
-  include ActionController::HttpAuthentication::Digest::ControllerMethods
-  USERS = { "jack" => Digest::MD5.hexdigest(["jack","Application","secret"].join(":"))}
+  # include ActionController::HttpAuthentication::Digest::ControllerMethods
+  # USERS = { "jack" => Digest::MD5.hexdigest(["jack","Application","secret"].join(":"))}
+
+  include ActionController::HttpAuthentication::Token::ControllerMethods
 
   before_action :authenticate
   before_action :set_kind, only: [:show, :update, :destroy]
@@ -62,8 +66,16 @@ class KindsController < ApplicationController
     end
 
     def authenticate
-      authenticate_or_request_with_http_digest("Application") do |username|
-        USERS[username]
+      # authenticate_or_request_with_http_digest("Application") do |username|
+      #   USERS[username]
+      # end
+      authenticate_or_request_with_http_token do |token, options|
+        # Compare the tokens in a time-constant manner, to mitigate
+        # timing attacks.
+        ActiveSupport::SecurityUtils.secure_compare(
+          ::Digest::SHA256.hexdigest(token),
+          ::Digest::SHA256.hexdigest(TOKEN)
+        )
       end
     end
 end
