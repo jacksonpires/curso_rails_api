aa0cbf314aebeb136a4905c813683c48bac6902b
Jackson Pires <jackson.pires@gmail.com>
2017-06-23 22:38:02 +0000

    Cache-Control, ETag e Last-Modified

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -11,7 +11,10 @@ module V1
 
       @contacts = Contact.all.page(page_number).per(per_page)
 
-      render  json: @contacts #, methods: :birthdate_br #[:hello, :i18n]
+      # Cache-Control --- expires_in 30.seconds, public: true
+      if stale?(etag: @contacts)
+        render  json: @contacts #, methods: :birthdate_br #[:hello, :i18n]
+      end
       # paginate json: @contacts #, methods: :birthdate_br #[:hello, :i18n]
     end
 
