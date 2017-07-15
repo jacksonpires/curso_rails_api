b28ec7763d4b9563567b1b06827d65a4801e0afe
Jackson Pires <jackson.pires@gmail.com>
2017-07-15 21:42:08 +0000

    HTTP Status Code e .rspec

--- spec/controllers/v1/contacts_spec.rb
+++ spec/controllers/v1/contacts_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe V1::ContactsController, type: :controller do
 
-  it 'request index and return 406 OK' do
+  it 'request index and return 406 NOT ACCEPTABLE' do
     get :index
-    expect(response).to have_http_status(406)
+    expect(response).to have_http_status(:not_acceptable)
   end
 
   it 'request index and return 200 OK' do
     request.accept = 'application/vnd.api+json'
     get :index
-    expect(response).to have_http_status(200)
+    expect(response).to have_http_status(:ok)
   end
 end
