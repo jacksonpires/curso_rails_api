759248d1565b497cccc4cabdd3effb25b305e74d
Jackson Pires <jackson.pires@gmail.com>
2017-07-16 00:09:04 +0000

    Melhorando a classe Hash

--- spec/controllers/v1/contacts_spec.rb
+++ spec/controllers/v1/contacts_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
+class Hash
+  def json(parts)
+    ary = parts.split(">")
+    ary.reduce(self) do |memo, key|
+      memo.fetch(key.to_s.strip) if memo
+    end
+  end
+end
+
 describe V1::ContactsController, type: :controller do
 
   it 'request index and return 406 NOT ACCEPTABLE' do
@@ -15,14 +24,13 @@ describe V1::ContactsController, type: :controller do
 
   it 'GET v1/contacts/:id' do
     contact = Contact.first
-
     request.accept = 'application/vnd.api+json'
     get :show, params: { id: contact.id }
-
     response_body = JSON.parse(response.body)
 
-    expect(response_body.fetch('data').fetch('id')).to eq(contact.id.to_s)
-    expect(response_body.fetch('data').fetch('type')).to eq('contacts')
+    expect(response_body.json('data > id')).to eq(contact.id.to_s)
+    expect(response_body.json('data > type')).to eq('contacts')
+    expect(response_body.json('data > attributes > name')).to eq(contact.name)
   end
 
 end
